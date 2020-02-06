171820ef3d21b23df52830c98a118833445c4490
davis <davisabr@gmail.com>
2020-02-05 23:28:15 -0700

    Added initial work for assignment 2 for 513

---
+++ SENG513/A2/calculator.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){ 
+
+    $(".operator").click( function() {
+        let currentText = $("#result").text();
+        
+        if (currentText.length > 20) {
+            return;
+        }
+        let operator = $(this)[0].value;
+
+        if (currentText !== "0" && currentText !== "ERROR") {
+            $("#result").text(currentText + operator);
+        } else if (operator === '(') {
+            $("#result").text(operator);
+        }
+    });
+
+    $(".digit").click(function() {
+        let currentText = $("#result").text();
+        if (currentText.length > 20) {
+            return;
+        }
+        let digit = $(this)[0].value;
+
+        if (currentText === "0" || currentText === "ERROR") {
+            $("#result").text(digit);
+        }
+        else {
+            $("#result").text(currentText + digit);
+        }
+    });
+
+    $(".backspace").click(function() {
+        let currentText = $("#result").text();
+
+        if (currentText !== '0' && currentText !== "ERROR") {
+            if (currentText.length === 1) {
+                currentText = '0';
+            } else {
+                currentText = currentText.slice(0, -1);
+            }
+            $("#result").text(currentText);
+        } else if (currentText === "ERROR") {
+            $("#result").text('0');
+        }
+    });
+
+    $(".clear").click(function() {
+        $("#result").text('0');
+    });
+
+    $(".decimal").click(function() {
+        let currentText = $("#result").text();
+        if (currentText.length > 20) {
+            return;
+        }
+        if (!currentText.includes('.')) {
+            // Append the decimal point
+            $("#result").text(currentText + '.');
+        }
+    })
+
+    $(".equals").click(function() {
+        let currentText = $("#result").text();
+        try {
+            currentText = currentText.replace("x", "*");
+            var result = eval(currentText);
+    
+            var decimalPlaces = result.toString().split('.');
+            if (decimalPlaces.length > 1) {
+                if (decimalPlaces[1].length > 5) {
+                    result = result.toFixed(5);
+                }
+            }
+    
+            if (result === Infinity) {
+                result = "ERROR";
+            }
+    
+            $("#result").text(result);
+        } catch(err) {
+            $("#result").text("ERROR");
+        }
+    });
+});
